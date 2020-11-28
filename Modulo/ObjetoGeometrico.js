601186d4d23155968b153cbc5f2c41fa168332b0
GermonSan <72879723+GermonSan@users.noreply.github.com>
2020-11-28 16:07:19 -0600

    hghg

--- Modulo/ObjetoGeometrico.js
+++ Modulo/ObjetoGeometrico.js
@@ -3,15 +3,26 @@ function ObjGeometrico (cuadrado, circulo){
     this.circulo=circulo
 }
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-  
-      ctx.fillRect(25,25,100,100);
-      ctx.clearRect(45,45,60,60);
-      ctx.strokeRect(50,50,50,50);
-    }
-  }
 
-function DbjCirculo (radio)
+function DBJCuadrado(cv,x,y,l) {
+    //var canvas = document.getElementById('canvas');
+    //if (canvas.getContext) {
+      //var ctx = canvas.getContext('2d');
+  
+      cv.ctx.fillRect(25,25,100,100);
+      cv.ctx.clearRect(45,45,60,60);
+      cv.ctx.strokeRect(50,50,50,50);
+   // }
+    
+  }
+  function DBJCirculo(cv,r){
+
+  }
+export { DBJCuadrado};
+
+class ObjGeo {
+
+  
+  
+}
+export {ObjGeo}
