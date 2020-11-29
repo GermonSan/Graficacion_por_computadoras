d1bece922e8084200762164508b0cfa658ff3a23
GermonSan <72879723+GermonSan@users.noreply.github.com>
2020-11-28 21:16:43 -0600

    p

--- Modulo/ObjetoGeometrico.js
+++ Modulo/ObjetoGeometrico.js
@@ -1,17 +1,28 @@
-function ObjGeometrico (cuadrado, circulo){
-    this.cuadrado=cuadrado
-    this.circulo=circulo
-}
+//clase 
+class ObjGeometrico{
+  constructor(x,y,l){
+    this.x=x;
+    this.y=y;
+    this.l=l;
+  }
+ 
 
-
-function DBJCuadrado(cv,x,y,l) {
+  //metodo
+   static DBJCuadrado(cv,x,y,l) {
     //var canvas = document.getElementById('canvas');
     //if (canvas.getContext) {
       //var ctx = canvas.getContext('2d');
       x=25;
       y=25;
       l=100;
-      cv.ctx.fillRect(x,y,l,l);
+      return cv.ctx.fillRect(x,y,l,l);
       
     }
-    export {DBJCuadrado};
+    //metodo
+     static DBJCirculo(cv,x,y,r){
+     return cv.ctx.arc(360,70,50,0,(Math.PI/180)*360,true);
+     cv.ctx.fillStyle="#f99";
+     cv.ctx.fill();
+    }
+}
+    export {DBJCuadrado,DBJCirculo,ObjGeometrico};
